4c14826ae2b441b792bdcce624868153f66d08d5
Muurein <amanda.l.thim@gmail.com>
2025-04-21 21:26:34 +0200

    added circle chart

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
 const dropdownMenuEl = document.getElementById("hamburger");
 const menuListEl = document.getElementById("nav-ul");
 const stapelEl = document.getElementById("barChart");
+const circleEl = document.getElementById("circleChart");
 let coursesPrograms = [];
 
 document.querySelectorAll(".menu-link").forEach(n => n.addEventListener("click", () => {
@@ -13,7 +14,6 @@ document.querySelectorAll(".menu-link").forEach(n => n.addEventListener("click",
 
 /* toggles menu */
 dropdownMenuEl.addEventListener ("click", () => {
-    console.log("clicked");
     dropdownMenuEl.classList.toggle("active");
     menuListEl.classList.toggle("active");
 })
@@ -91,7 +91,7 @@ function writeOutCourses() {
     //sortera fram vilka värden i coursesProgram som har typen "kurs", lägg dem den nya arrayen courses
     let courses = coursesPrograms.filter((course) => course.type === "Kurs");
 
-    //sortera fram vilka kurser som har högst antal sökande, lägg dessa i den nya arrayen 
+    //sortera fram vilka kurser som har flest antal sökande, lägg dessa i den nya arrayen 
     let sortedCourses = courses.sort((courseA, courseB) => courseB.applicantsTotal - courseA.applicantsTotal);
 
     //array för att lagra de 6 kurser med flest applicantsTotal
@@ -101,10 +101,10 @@ function writeOutCourses() {
         coursesMostApplicants.push(sortedCourses[i]);
     } 
 
-    //randomiserar de sorterade värdena så stapeldiagrammet blir roligare att titta på - BLIR FÖR ROLIGT TA BORT??
-    coursesMostApplicants.sort(() => Math.random() - 0.5);
+    //randomiserar de sorterade värdena så stapeldiagrammet blir roligare att titta på
+    //coursesMostApplicants.sort(() => Math.random() - 0.5);
+    //tog bort detta för det blev lite för roligt
 
-    console.table(coursesMostApplicants); //lägg in varje värde i egen array?
 
     //lägg in varje kursnamn i varsina arrayer för att kunna lägga in arrayerna som värden i x-axeln i stapeldiagrammet
     let courseName1 = [];
@@ -142,10 +142,10 @@ function writeOutCourses() {
     new Chart(stapelEl, { //varför står det "1399, null" i resultaten när man hovrar på staplarna????
         type: "bar",
         data: {
-            labels: [courseName1, courseName2, courseName3, courseName4, courseName5, courseName6],
+            labels: [courseName3, courseName5, courseName1, courseName2, courseName6, courseName4],
             datasets: [{
-                label: "antal sökande",
-                data: [courseApplicants1, courseApplicants2, courseApplicants3, courseApplicants4, courseApplicants5, courseApplicants6],
+                label: "antal sökande till kurser",
+                data: [courseApplicants3, courseApplicants5, courseApplicants1, courseApplicants2, courseApplicants6, courseApplicants4],
                 borderWidth: 1
             }]
         },
@@ -162,5 +162,65 @@ function writeOutCourses() {
 }
 
 function writeOutPrograms() {
-    
+
+    //sortera fram vilka värden i coursesPrograms som har typen "programs", lägg dem i nya arrayen programs
+    let programs = coursesPrograms.filter((program) => program.type === "Program");
+
+    //sortera fram vilka program som har högst antal
+    let sortedPrograms = programs.sort((programA, programB) => programB.applicantsTotal - programA.applicantsTotal);
+
+    //array för att lagra de 6 program som har flest sökande
+    let programsMostApplicants = [];
+
+    for(let i = 0; i < 6; i++) {
+        programsMostApplicants.push(sortedPrograms[i]);
+    }
+
+    console.table(programsMostApplicants); //lägg in varje värde i egen array?
+
+    //lägg in varje programs namn i varsina arrayer för att kunna lägga in arrayerna som "titel-värden" i cirkeldiagrammet
+    let programName1 = [];
+    let programName2 = [];
+    let programName3 = [];
+    let programName4 = [];
+    let programName5 = [];
+    let programName6 = [];
+
+    programName1.push(programsMostApplicants[0].name);
+    programName2.push(programsMostApplicants[1].name);
+    programName3.push(programsMostApplicants[2].name);
+    programName4.push(programsMostApplicants[3].name);
+    programName5.push(programsMostApplicants[4].name);
+    programName6.push(programsMostApplicants[5].name); 
+
+    //lägg in varje applicantsTotal i varsina arrayer för att kunna lägga in arrayerna som värden "antal-värden" i cirkeldiagrammet
+    let programApplicants1 = [];
+    let programApplicants2 = [];
+    let programApplicants3 = [];
+    let programApplicants4 = [];
+    let programApplicants5 = [];
+    let programApplicants6 = [];
+
+    programApplicants1.push(programsMostApplicants[0].applicantsTotal);
+    programApplicants2.push(programsMostApplicants[1].applicantsTotal);
+    programApplicants3.push(programsMostApplicants[2].applicantsTotal);
+    programApplicants4.push(programsMostApplicants[3].applicantsTotal);
+    programApplicants5.push(programsMostApplicants[4].applicantsTotal);
+    programApplicants6.push(programsMostApplicants[5].applicantsTotal);
+
+
+    new Chart(circleEl, {
+        type: "pie",
+        data: {
+            labels: [programName1, programName2, programName3, programName4, programName5, programName6],
+            datasets: [{
+                label: "antal sökande till program",
+                data: [programApplicants1, programApplicants2, programApplicants3, programApplicants4, programApplicants5, programApplicants6],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            backgroundColor: ["Red", "Yellow", "Green", "Pink", "Blue", "Purple"]
+        }
+    });
 }
